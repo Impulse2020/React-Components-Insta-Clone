f8a5e15eb75aa56cd0687c11970a3c6337d68c75
Impulse2020 <irishht@gmail.com>
2020-05-19 16:31:09 -0400

    Adds comment component

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,19 +3,18 @@
 import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data"
 // import data 
 
 const PostsPage = () => {
-  const [posts, setPosts] = useState('');
+  const [posts, setPosts] = useState(dummyData);
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
-      {/*Post.map(thing => 
-        
-        <Post thumbnailUrl="" comments="" imageUrl="" username="" />
-        
-      )*/}
+      
+      
+
     </div>
   );
 };
